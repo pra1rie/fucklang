f3184427329f3f0fa3ccb4b155dcd767f3ecd400
Ren <73724137+pra1rie@users.noreply.github.com>
2024-06-18 11:33:25 +0100

    rename lib/stdio -> lib/stdlib

--- build_stdlib.sh
+++ build_stdlib.sh
@@ -2,5 +2,5 @@
 set -xe
 
 dmd -c -of=lib/core.o core/*.d src/*.d -I=src/
-tcc -O2 -c -o lib/std.o lib/*.c -I core/
-dmd -O -of=lib/stdio.so lib/*.o -shared -fPIC
+gcc -O3 -c -o lib/std.o lib/*.c -I core/
+dmd -O -of=lib/stdlib.so lib/*.o -shared -fPIC
